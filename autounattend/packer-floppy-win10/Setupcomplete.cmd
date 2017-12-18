0071e7519202889cc1be3465a7d695d96d3684b3
arkady <larytet@yahoo.com>
2017-12-18 21:14:22 +0200

    Setupcomplete.cmd

--- autounattend/packer-floppy-win10/Setupcomplete.cmd
+++ autounattend/packer-floppy-win10/Setupcomplete.cmd
@@ -1,2 +1,2 @@
-"C:\Windows\Temp\setup-x86.exe" -q -R C:\cygwin -l C:\Windows\Temp --packages "wget,zip,screen" -s http://cygwin.mirror.constant.com"
+"C:\Windows\Temp\setup-x86.exe" -q -R C:\cygwin -l C:\Windows\Temp --packages "wget,zip,screen" -s http://cygwin.mirror.constant.com
 
