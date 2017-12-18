190bd7793bd2073a55c141d7257c761e4c7158f2
arkady <larytet@yahoo.com>
2017-12-18 14:52:52 +0200

    Send ping to the host upon start

--- test_vm.py
+++ test_vm.py
@@ -205,7 +205,7 @@ class TestInstallVm:
             presents, machine = self.__vm_presents(os_name, architecture)
             mac = vbox.get_mac_address(machine.uuid)
             uuids_macs[machine.uuid] = mac
-        print("Waiting for ping from {uuids_macs}")
+        print(f"Waiting for ping from {uuids_macs}")
         while (datetime.datetime.now() < time_end):
             if len(uuids_macs) == 0:
                 break
@@ -213,7 +213,7 @@ class TestInstallVm:
             mac = uuids_macs[uuid]
             res, hostname, ipaddress = utils.find_ip_by_mac(mac)
             if res:
-                print("Got IP {ipaddress} {hostname} for {mac}")
+                print(f"Got IP {ipaddress} {hostname} for {mac}")
                 del uuids_macs[uuid]
             time.sleep(5.0)
 
