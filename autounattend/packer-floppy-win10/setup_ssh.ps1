4b863e6f5d4e6919e5e69bb2c485a02c3cd47d43
arkady <larytet@yahoo.com>
2017-12-20 17:56:42 +0200

    switch to cygwin sshd fix bash script

--- autounattend/packer-floppy-win10/setup_ssh.ps1
+++ autounattend/packer-floppy-win10/setup_ssh.ps1
@@ -1,12 +1,14 @@
-# configure firewall
-
 Write-Output "Configuring firewall"
 New-NetFirewallRule -Protocol TCP -LocalPort 22 -Direction Inbound -Action Allow -DisplayName SSH
 New-NetFirewallRule -Protocol TCP -LocalPort 22 -Direction Outbound -Action Allow -DisplayName SSH
 
-# Create a home directory for the user
+Write-Output "Create a home directory for the user"
 & "mkdir" C:\cygwin\home\user
 
+# The trick here is to start a "screen" session with DISPLAY correctly set 
+# and allow starting GUI applications remotely
+# This cmd file will invoke Cygwin bash with screen
+Write-Output "Create C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start_screen.cmd"
 $StartScreenBat = @"
 @echo off 
 C:
@@ -19,6 +21,7 @@ sleep 30
 $StartScreenBat | Out-File -FilePath "C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start_screen.cmd" -Encoding ASCII
 Start-Sleep -s 30
 
+Write-Output "Create input file for ssh-host-config C:\cygwin\home\user\password"
 $SshConfigInput = @"
 user
 user
@@ -26,6 +29,9 @@ user
 $SshConfigInput | Out-File -FilePath "C:\cygwin\home\user\password" -Encoding ASCII
 Start-Sleep -s 30
 
+# I iwll run this BASH script every time user logins
+# This is a real Linux bash script with Unix end of line 
+Write-Output "C:\cygwin\home\user\start_screen.sh"
 $StartScreenSh = @"
 #!/bin/bash
 ssh-host-config -y < /home/user/password
