03d76a848827838fa0d57e40f71243febca1e55a
arkady <larytet@yahoo.com>
2017-12-20 17:16:25 +0200

    switch to cygwin sshd

--- autounattend/packer-floppy-win10/setup_ssh.ps1
+++ autounattend/packer-floppy-win10/setup_ssh.ps1
@@ -1,27 +1,36 @@
 # configure firewall
+
 Write-Output "Configuring firewall"
-netsh advfirewall firewall add rule name="SSH port 22" dir=in action=allow protocol=TCP localport=22
-netsh advfirewall firewall add rule name="SSH port 22" dir=out action=allow protocol=TCP localport=22
+New-NetFirewallRule -Protocol TCP -LocalPort 22 -Direction Inbound -Action Allow -DisplayName SSH
+New-NetFirewallRule -Protocol TCP -LocalPort 22 -Direction Outbound -Action Allow -DisplayName SSH
 
-
-
+# Create a home directory for the user
+& "mkdir" C:\cygwin\home\user
 
 $StartScreenBat = @"
 @echo off 
 C:
-mkdir C:\cygwin\home\user
 chdir C:\cygwin\bin
 set HOME=\cygwin\home\user
+echo Calling bash
 bash --login -i `"/home/user/start_screen.sh`"
+sleep 30
 "@
 $StartScreenBat | Out-File -FilePath "C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start_screen.cmd" -Encoding ASCII
+Start-Sleep -s 30
+
+$SshConfigInput = @"
+user
+user
+"@
+$SshConfigInput | Out-File -FilePath "C:\cygwin\home\user\password" -Encoding ASCII
+Start-Sleep -s 30
 
 $StartScreenSh = @"
 #!/bin/bash
-echo user > password
-echo user >> password
-ssh-host-config -y < password
+ssh-host-config -y < /home/user/password
 cygrunsrv -S sshd
 screen -dmS `"main`"
 "@
 $StartScreenSh | Out-File -FilePath "C:\cygwin\home\user\start_screen.sh" -Encoding ASCII
+Start-Sleep -s 30
